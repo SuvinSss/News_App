b3ab5f693f8d0f332a8bf1f884bf0afd98f32c89
Suvin <websoullabssuvin@gmail.com>
2022-01-16 21:00:30 +0530

    App Created and Registered

--- main/models.py
+++ main/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 # Create your models here.
 class Main(models.Model):
-    name = models.TextField
+    name = models.TextField(default="")
 
     def __str__(self):
         return self.name
